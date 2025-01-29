752b0066abee1b990fdcc3e3ada547ac31bdc2e2
anilyavas <anil.yavas99@gmail.com>
2025-01-29 18:21:25 +0300

    edge functions webhooks

--- supabase/functions/trigger_collection_api/index.ts
+++ supabase/functions/trigger_collection_api/index.ts
@@ -1,11 +1,8 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
-// Setup type definitions for built-in Supabase Runtime APIs
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts';
 
 console.log('Hello from Functions!');
+
+//https://api.brightdata.com/datasets/v3/trigger?dataset_id=gd_lk538t2k2p1k3oos71&endpoint=https://fetezusfowkehtdpfipi.supabase.co/functions/v1/collection_webhook&format=json&uncompressed_webhook=true&include_errors=true
 
 Deno.serve(async (req: any) => {
   const { url } = await req.json();
@@ -13,7 +10,7 @@ Deno.serve(async (req: any) => {
   console.log(url);
 
   const response = await fetch(
-    `https://api.brightdata.com/datasets/v3/trigger?dataset_id=gd_lk538t2k2p1k3oos71&include_errors=true`,
+    `https://api.brightdata.com/datasets/v3/trigger?dataset_id=gd_lk538t2k2p1k3oos71&endpoint=${Deno.env.get('SUPABASE_URL')}/functions/v1/collection_webhook&format=json&uncompressed_webhook=true&include_errors=true`,
     {
       headers: {
         Authorization: `Bearer ${Deno.env.get('BRIGHT_DATA_API_KEY')}`,
@@ -38,15 +35,3 @@ Deno.serve(async (req: any) => {
 
   return new Response(JSON.stringify(data), { headers: { 'Content-Type': 'application/json' } });
 });
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/trigger_collection_api' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
